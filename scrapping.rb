52b870d915a6ad0525d883d410366ec838ff9f79
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-18 09:03:38 -0500

    Descargar archivos desde codigo

--- scrapping.rb
+++ scrapping.rb
@@ -8,8 +8,14 @@ url = 'http://www.reactiongifs.com/'
   document = Nokogiri::HTML(open(url))
   div_main = document.css('div#main')
   div_main.css('div.post').each do |post|
-    puts post.css('h2').text
-    puts post.css('div.entry img').attr('src')
+    title =  post.css('h2').text
+    gif_url = post.css('div.entry img').attr('src')
+    File.open(title, 'w') do |new_file|
+      puts "Descargando gif: #{title}"
+      open(gif_url, 'r') do |gif|
+        new_file.write(gif.read)
+      end
+    end
   end
   document.css('div.nav-entries div.nav-next a').attr('href')
 end
