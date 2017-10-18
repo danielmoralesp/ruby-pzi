f2f4d1804eff4b132216fd3f639f29f3ddb5044c
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-18 09:45:45 -0500

    Consumir api con httparty

--- ruby_web/application.rb
+++ ruby_web/application.rb
@@ -1,10 +1,14 @@
 require 'erb'
+require 'httparty'
 
 class Application
   def self.call(env)
     if env['PATH_INFO'] == '/'
+      url = 'http://jsonplaceholder.typicode.com/posts'
+      response = HTTParty.get(url)
+      @posts = JSON.parse(response.body)
       body = ERB.new(File.read('index.html.erb'))
-      [200, {}, [body.result]]
+      [200, {}, [body.result(binding)]]
     else
       [404, {}, ['No existe esta pagina']]
     end
