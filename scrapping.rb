05e0b0661f617c5b789f89ccf898fada59e72a81
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-18 08:48:19 -0500

    Extraer texto e imagenes

--- scrapping.rb
+++ scrapping.rb
@@ -2,7 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 url = 'http://www.reactiongifs.com/'
-
 document = Nokogiri::HTML(open(url))
-
-puts document
+div_main = document.css('div#main')
+div_main.css('div.post').each do |post|
+  puts post.css('h2').text
+  puts post.css('div.entry img').attr('src')
+end
