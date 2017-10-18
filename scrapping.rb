52a60458ec23cd708143cfcdc5eb328b28ccd9a3
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-18 08:51:54 -0500

    Ir por la paginacion

--- scrapping.rb
+++ scrapping.rb
@@ -2,9 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 url = 'http://www.reactiongifs.com/'
-document = Nokogiri::HTML(open(url))
-div_main = document.css('div#main')
-div_main.css('div.post').each do |post|
-  puts post.css('h2').text
-  puts post.css('div.entry img').attr('src')
+
+5.times do |i|
+  puts "Pagina #{i}"
+  document = Nokogiri::HTML(open(url))
+  div_main = document.css('div#main')
+  div_main.css('div.post').each do |post|
+    puts post.css('h2').text
+    puts post.css('div.entry img').attr('src')
+  end
+  document.css('div.nav-entries div.nav-next a').attr('href')
 end
